62ea15e301ac56b269b9ac6b16da2b8a1e39c7b5
Jasowills <Jujuzhaddy01@gmail.com>
2022-09-29 21:39:42 +0100

    origin

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function dropdown() {
+    
+    let bars = document.getElementsById('bars')
+    bars.style.display = 'none'
+}
+function clicker() {
+    let secondNav = document.getElementById('secondnav')
+    secondNav.style.display = 'block'
+    let icon = document.getElementById('icon')
+    icon.style.display = 'none'
+    let icon2 = document.getElementById('icon2')
+    icon2.style.display = 'block'
+}
+function cancel() {
+    let secondNav = document.getElementById('secondnav')
+    secondNav.style.display = 'none'
+    let icon = document.getElementById('icon')
+    icon.style.display = 'block'
+    let icon2 = document.getElementById('icon2')
+    icon2.style.display = 'none'
+}
